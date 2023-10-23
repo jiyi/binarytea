2f8779e438482b36e6d13830359cc1f4f87cae1b
Jiyi <jiyixx@gmail.com>
2023-10-23 14:56:41 +0800

    完成4.4.1，新建项目，测试配置类

---
+++ src/test/java/learning/spring/binarytea/BinaryTeaApplicationTests.java
@@ -0,0 +1,12 @@
+package learning.spring.binarytea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BinaryTeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
