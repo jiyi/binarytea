f11f76f0b41953578415befed6fc7ea80302e2ae
Jiyi <jiyilc.sd@chinatelecom.cn>
2023-12-23 16:00:57 +0800

    删除其他

--- src/main/java/learning/spring/binarytea/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package learning.spring.binarytea.repository;
-
-import learning.spring.binarytea.model.MenuItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<MenuItem, Long> {
-}
--- src/main/java/learning/spring/binarytea/repository/TeaMakerRepository.java
+++
@@ -1,7 +0,0 @@
-package learning.spring.binarytea.repository;
-
-import learning.spring.binarytea.model.TeaMaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeaMakerRepository extends JpaRepository<TeaMaker, Long> {
-}
