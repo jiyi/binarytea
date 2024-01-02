7d2cb674fb237868f4f24f17f4df9e6bca79e746
Jiyi <jiyilc.sd@chinatelecom.cn>
2024-01-02 16:41:08 +0800

    完成8.3.1

--- src/main/java/learning/spring/binarytea/runner/MenuPrinterRunner.java
+++ src/main/java/learning/spring/binarytea/runner/MenuPrinterRunner.java
@@ -1,13 +1,12 @@
 package learning.spring.binarytea.runner;
 
 import learning.spring.binarytea.model.MenuItem;
-import learning.spring.binarytea.repository.MenuRepository;
+import learning.spring.binarytea.service.MenuService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
-import org.springframework.data.redis.core.BoundListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,32 +15,14 @@ import java.util.List;
 @Slf4j
 @Order(2)
 public class MenuPrinterRunner implements ApplicationRunner {
-
-    private final MenuRepository menuRepository;
-    private final RedisTemplate redisTemplate;
-
-    public MenuPrinterRunner(MenuRepository menuRepository, RedisTemplate redisTemplate) {
-        this.menuRepository = menuRepository;
-        this.redisTemplate = redisTemplate;
-    }
+    @Autowired
+    private MenuService menuService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        long size = 0;
-        List<MenuItem> menuItemList = null;
-        if (redisTemplate.hasKey("binarytea-menu")) {
-            BoundListOperations<String, MenuItem> operations = redisTemplate.boundListOps("binarytea-menu");
-            size = operations.size();
-            menuItemList = operations.range(0, -1);
-            log.info("Loading menu from Redis.");
-        } else {
-            size = menuRepository.count();
-            menuItemList = menuRepository.findAll();
-            log.info("Loading menu from DB.");
-        }
-
-        log.info("共有{}个饮品可选。", size);
-        menuItemList
-                .forEach(i -> log.info("饮品：{}", i));
+        log.info("再次加载菜单列表，应该不会访问数据库");
+        List<MenuItem> list = menuService.getAllMenu();
+        log.info("共有{}个饮品可选", list.size());
+        list.forEach(i -> log.info("重新查询菜单项({})：{}", i.getId(), menuService.getByNameAndSize(i.getName(), i.getSize())));
     }
 }
